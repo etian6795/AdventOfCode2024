c7294b7e30dc05259131f839694bcd2cb684e86b
etian6795 <evantian05@gmail.com>
2024-12-01 00:12:59 -0500

    Day 1 Solutions

---
+++ Day1P1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Day1P1 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int n = 1000;
+        int[][] arr = new int[2][n];
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            arr[0][i] = Integer.parseInt(s.substring(0, 5));
+            arr[1][i] = Integer.parseInt(s.substring(8));
+        }
+        for(int[] x : arr) Arrays.sort(x);
+        for(int i = 0; i < n; i++) ans += Math.abs(arr[0][i] - arr[1][i]);
+        out.println(ans);
+
+        out.close();
+    }
+}
---
+++ Day1P2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+
+public class Day1P2 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int n = 1000;
+        int[][] arr = new int[2][n];
+        long ans = 0;
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            arr[0][i] = Integer.parseInt(s.substring(0, 5));
+            arr[1][i] = Integer.parseInt(s.substring(8));
+        }
+        long[] f = new long[1000000];
+        for(int i = 0; i < n; i++) f[arr[1][i]]++;
+        for(int i = 0; i < n; i++) ans += arr[0][i] * f[arr[0][i]];
+        out.println(ans);
+
+        out.close();
+    }
+}
