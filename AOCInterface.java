ed15736a2009158b2fd74009fd9e9a1bffedac12
etian6795 <evantian05@gmail.com>
2024-12-09 23:58:40 -0500

    Updated AOCInterface

--- AOCInterface.java
+++ AOCInterface.java
@@ -1,22 +1,18 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public interface AOCInterface {
     public static long[] readLineAsLongs(String s) {
         ArrayList<Long> arr = new ArrayList<>();
-        int idx = 0;
-        int n = s.length();
-        while(idx < n) {
-            long curr = 0;
-            while(idx < n && Character.isDigit(s.charAt(idx))) {
-                curr = curr * 10 + (s.charAt(idx)-'0');
-                idx++;
-            }
-            arr.add(curr);
-            while(idx < n && !Character.isDigit(s.charAt(idx))) {
-                idx++;
-            }
+        
+        Pattern pattern = Pattern.compile("-?\\d+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            arr.add(Long.valueOf(matcher.group()));
         }
+
         long[] res = new long[arr.size()];
         for(int i = 0; i < res.length; i++) {
             res[i] = arr.get(i);
