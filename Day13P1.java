287e078b12fbd0cdbffb3f1d6bdb45d95b1bf98b
etian6795 <evantian05@gmail.com>
2024-12-13 00:42:58 -0500

    Day 13 Solutions

---
+++ Day13P1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Day13P1 implements AOCInterface {
+    public static BufferedReader br;
+    public static PrintWriter out = new PrintWriter(System.out);
+    public static String file = "Day13Input.txt";
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new FileReader(file));
+        ArrayList<Integer> qwerty;
+
+        String line;
+        long ans = 0;
+        while((line = br.readLine()) != null) {
+            long[] A = AOCInterface.readLineAsLongs(line);
+            line = br.readLine();
+            long[] B = AOCInterface.readLineAsLongs(line);
+            line = br.readLine();
+            long[] P = AOCInterface.readLineAsLongs(line);
+            line = br.readLine();
+
+            long temp = Long.MAX_VALUE;
+            for(int i = 0; i <= 100; i++) {
+                for(int j = 0; j <= 100; j++) {
+                    if(A[0]*i + B[0]*j == P[0] && A[1]*i + B[1]*j == P[1]) {
+                        temp = Math.min(temp, i*3+j);
+                    }
+                }
+            }
+            if(temp != Long.MAX_VALUE) {
+                ans += temp;
+            }
+        }
+        
+        out.println(ans);
+        out.close();
+    }
+}
---
+++ Day13P2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Day13P2 implements AOCInterface {
+    public static BufferedReader br;
+    public static PrintWriter out = new PrintWriter(System.out);
+    public static String file = "Day13Input.txt";
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new FileReader(file));
+
+        String line;
+        long ans = 0;
+        while((line = br.readLine()) != null) {
+            long[] A = AOCInterface.readLineAsLongs(line);
+            line = br.readLine();
+            long[] B = AOCInterface.readLineAsLongs(line);
+            line = br.readLine();
+            long[] P = AOCInterface.readLineAsLongs(line);
+            P[0] += 10000000000000l;
+            P[1] += 10000000000000l;
+            line = br.readLine();
+            
+            long a = A[0];
+            long b = B[0];
+            long c = A[1];
+            long d = B[1];
+            long e = P[0];
+            long f = P[1];
+
+            double det = (a*d - b*c);
+            long x = (long)(((d) * (e) - (b) * (f)) / det);
+            long y = (long)(((a) * (f) - (c) * (e)) / det);
+            x = Math.abs(x);
+            y = Math.abs(y);
+            if(x*a + y*b == e && x*c + y*d == f) {
+                ans += 3*x+y;
+            }
+        }
+        
+        out.println(ans);
+        out.close();
+    }
+}
